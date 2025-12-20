3a693c6e5f8f7249939eecdb7eda003bb8701496
Avi Espa <avi05espa@gmail.com>
2025-12-20 10:07:15 +0000

    Y eso porque sucede 😤

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,12 +4,12 @@
 import { FirebaseOptions, getApp, getApps, initializeApp } from 'firebase/app';
 
 const firebaseConfig: FirebaseOptions = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "API_KEY",
+  authDomain: "PROJECT_ID.firebaseapp.com",
+  projectId: "PROJECT_ID",
+  storageBucket: "PROJECT_ID.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID"
 };
 
 
