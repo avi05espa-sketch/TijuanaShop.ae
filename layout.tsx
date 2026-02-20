87cdcea89cbcd2c30d2c84305794a27ae67f8d0c
avi05espa-sketch <avi05espa@gmail.com>
2026-02-19 16:15:35 -0800

    Update layout.tsx

--- layout.tsx
+++ layout.tsx
@@ -1,12 +1,1 @@
-'use client';
-import React from "react";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="es">
-      <body>
-        {children}
-      </body>
-    </html>
-  );
-}
