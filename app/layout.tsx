4773d449acb75d800e13ac85d2f846bb701c8145
avi05espa-sketch <avi05espa@gmail.com>
2026-02-19 19:07:45 -0800

    Actualizar layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,9 @@
 'use client';
 import React from "react";
-import { usePathname } from "next/navigation";
-import "./globals.css";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  const pathname = usePathname();
-
   return (
     <html lang="es">
-      <head>
-        <title>Tijuana Shop | Tu Marketplace Local en Línea</title>
-      </head>
       <body>
         {children}
       </body>
