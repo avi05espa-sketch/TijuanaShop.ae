b4850c27dbe4be9f1e7e1396f61f5fcb61753cca
avi05espa-sketch <avi05espa@gmail.com>
2026-02-28 03:47:15 -0800

    Crear data/mockData.ts

---
+++ src/data/mockData.ts
@@ -0,0 +1,14 @@
+export const categories = [
+  "Todos",
+  "Electrónica",
+  "Ropa y Calzado",
+  "Hogar",
+  "Juguetes",
+  "Mascotas",
+  "Belleza",
+  "Herramientas",
+  "Otros"
+];
+
+// Datos de ejemplo por si la base de datos está vacía al inicio
+export const MOCK_PRODUCTS = []; 
